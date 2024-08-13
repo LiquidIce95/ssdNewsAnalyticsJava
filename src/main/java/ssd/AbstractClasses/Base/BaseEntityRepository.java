29d1b43aa3c6e8e8f654bff38510bfb442a03e99
LiquidIce95 <davidsanchez062@gmail.com>
2024-08-13 10:52:27 +0200

    made some minor code more explicit

--- src/main/java/ssd/AbstractClasses/Base/BaseEntityRepository.java
+++ src/main/java/ssd/AbstractClasses/Base/BaseEntityRepository.java
@@ -3,11 +3,13 @@ package ssd.AbstractClasses.Base;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import ssd.AbstractClasses.Analytics.Analytics;
+
 import java.util.List;
 import java.util.Optional;
 
 @NoRepositoryBean
-public interface BaseEntityRepository<T extends BaseEntity<?>> extends JpaRepository<T, Long> {
+public interface BaseEntityRepository<T extends BaseEntity<? extends Analytics>> extends JpaRepository<T, Long> {
 
     List<T> findByName(String name);
 
